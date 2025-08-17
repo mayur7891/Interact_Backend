62a5859c746f61be017fcca0dde82a6b22c7b8b1
mayur7891 <113599835+mayur7891@users.noreply.github.com>
2025-08-17 22:06:58 +0530

    GCP

--- app/model.py
+++ app/model.py
@@ -19,9 +19,9 @@ class CommentModel:
             "video_id": video_id,
             "user_id": user_id,
             "comment": comment_text,
-            "embedding": embedding,  # List of floats
+            "embedding": embedding,  
             "cluster": cluster,
-            "sentiment": sentiment,  # "positive", "negative", "neutral"
+            "sentiment": sentiment,  
             "replies": [],
             "timestamp": timestamp
         }
@@ -35,7 +35,7 @@ class CommentModel:
                                     .sort("timestamp", -1) 
         return [{**comment, "_id": str(comment["_id"])} for comment in comments]
 
-    @staticmethod # needs to be updated (video and comment id)
+    @staticmethod 
     def add_reply(comment_id, reply_user_id, reply_text, reply_id):
         """Add a reply to a specific comment."""
         reply_data = {
@@ -47,22 +47,22 @@ class CommentModel:
             {"_id": ObjectId(comment_id)},
             {"$push": {"replies": reply_data}}
         )
-        return reply_id  # Returns True if the update was successful
+        return reply_id  
 
     @staticmethod
     def get_replies(comment_id):
         """Fetch the replies list of a specific comment by _id."""
         comment = mongo.db.comments.find_one({"_id": ObjectId(comment_id)}, {"replies": 1})
         if comment:
-            return comment.get("replies", [])  # Return replies list or empty if none
-        return None  # Return None if comment not found
+            return comment.get("replies", []) 
+        return None 
 
     @staticmethod
     def get_comment_by_id(comment_id):
         """Fetch a single comment by ID."""
         comment = mongo.db.comments.find_one({"_id": ObjectId(comment_id)})
         if comment:
-            comment["_id"] = str(comment["_id"])  # Convert ObjectId to string
+            comment["_id"] = str(comment["_id"]) 
         return {**comment, "_id": str(comment["_id"])}
     
     @staticmethod
@@ -71,7 +71,7 @@ class CommentModel:
         comments_cursor = mongo.db.comments.find(
             {"video_id": video_id, "embedding": {"$exists": True, "$ne": []}}
         )
-        comments = list(comments_cursor)  # Convert cursor to a list
+        comments = list(comments_cursor) 
 
         return [{**comment, "_id": str(comment["_id"])} for comment in comments]
 
@@ -108,16 +108,16 @@ class CommentModel:
     def get_unique_clusters_with_sample(video_id):
         """Fetch unique clusters and one comment from each cluster for a specific video."""
         pipeline = [
-            {"$match": {"video_id": video_id}},  # Filter by video_id
+            {"$match": {"video_id": video_id}},  
             {"$group": {
                 "_id": "$cluster",
-                "sample_comment": {"$first": "$$ROOT"}  # Pick the first comment per cluster
+                "sample_comment": {"$first": "$$ROOT"}  
             }},
             {"$project": {
                 "_id": 0,
                 "cluster": "$_id",
                 "comment": {
-                    "_id": {"$toString": "$sample_comment._id"},  # Convert ObjectId to string
+                    "_id": {"$toString": "$sample_comment._id"},  
                     "video_id": "$sample_comment.video_id",
                     "user_id": "$sample_comment.user_id",
                     "comment": "$sample_comment.comment",
@@ -147,9 +147,9 @@ class SentimentModel:
         }}
 
         result = mongo.db.sentimentDetail.update_one(
-            {"video_id": video_id},  # Match the video ID
+            {"video_id": video_id},  
             sentiment_data,
-            upsert=True  # If not found, insert a new entry
+            upsert=True  
         )
         return result.upserted_id if result.upserted_id else "Updated"
     
@@ -158,7 +158,7 @@ class SentimentModel:
         """Fetch sentiment data for a given video ID."""
         sentiment = mongo.db.sentimentDetail.find_one(
             {"video_id": video_id}, 
-            {"_id": 0, "positive": 1, "negative": 1, "neutral": 1}  # Exclude _id, video_id
+            {"_id": 0, "positive": 1, "negative": 1, "neutral": 1}  
         )
         return sentiment if sentiment else None
     
@@ -184,18 +184,18 @@ class VideoModel:
             result = mongo.db.videos.insert_one(video_data)
             return {"message": "Video added successfully", "video_id": str(result.inserted_id)}
         except DuplicateKeyError:
-            return {"error": "video_id already exists"}  # Prevent duplicate insertion
+            return {"error": "video_id already exists"}  
         
     @staticmethod
     def get_all_videos():
         """Fetches all videos from the database"""
-        videos = mongo.db.videos.find({}, {"_id": 0})  # Excluding MongoDB's default _id field
+        videos = mongo.db.videos.find({}, {"_id": 0})  
         return list(videos)
 
     @staticmethod
     def get_video_by_id(video_ID):
         """Fetches a single video by video_id"""
-        video = mongo.db.videos.find_one({"video_id": video_ID}, {"_id": 0})  # Exclude _id field
+        video = mongo.db.videos.find_one({"video_id": video_ID}, {"_id": 0})  
 
         if video:
             return video
@@ -209,8 +209,8 @@ class ClusterReplyModel:
     def add_cluster_reply(cluster_no,video_id,creator_id,reply_text,timestamp):
         query = {"video_id": video_id, "cluster_no": cluster_no}
         update = {
-            "$push": {"replies": {"reply_text":reply_text,"timestamp":timestamp}},  # Append new reply
-            "$setOnInsert": {"creator_id": creator_id,}  # Set only on insert
+            "$push": {"replies": {"reply_text":reply_text,"timestamp":timestamp}},  
+            "$setOnInsert": {"creator_id": creator_id,}  
         }
         options = {"upsert": True}
 
@@ -260,7 +260,7 @@ class UserModel:
         """Generates a JWT token for authenticated users"""
         payload = {
             "user_name": user_name,
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2)  # Token expires in 2 hours
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2) 
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
         return token
@@ -294,7 +294,7 @@ class UserModel:
         """Registers a new user with a hashed password & unique username"""
         user_data = {
             "user_name": user_name,
-            "password": generate_password_hash(password),  # Hash password before storing
+            "password": generate_password_hash(password),  
             "isCreator": is_creator
         }
 
@@ -307,7 +307,7 @@ class UserModel:
     @staticmethod
     def get_user(user_name):
         """Fetches a user by user_name"""
-        user = mongo.db.users.find_one({"user_name": user_name}, {"_id": 0, "password": 0})  # Hide password
+        user = mongo.db.users.find_one({"user_name": user_name}, {"_id": 0, "password": 0})  
 
         if user:
             return user
@@ -343,7 +343,7 @@ class SummaryModel:
         mongo.db.videoSummary.update_one(
             {"video_id": video_id},
             {"$set": {"summary": new_summary}},
-            upsert=True  # If no summary exists, create a new one
+            upsert=True  
         )
         return {"message": "Summary updated successfully"}
 
@@ -352,7 +352,7 @@ class SummaryModel:
         """Fetches the summary for a given video."""
         summary = mongo.db.videoSummary.find_one(
             {"video_id": video_id},
-            {"_id": 0, "summary": 1}  # Exclude _id field
+            {"_id": 0, "summary": 1} 
         )
         return summary if summary else {"message": "No summary available"}
     
