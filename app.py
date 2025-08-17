ac0b910d7fc7a61e16c2af347e450ea88a76f1cd
mayur7891 <113599835+mayur7891@users.noreply.github.com>
2025-08-17 22:04:22 +0530

    GCP

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from app import create_app  
 import os
 from app import socketio
-# from dotenv import load_dotenv  # ✅ Load .env file
 
-# # Load environment variables from .env
-# load_dotenv()
 
 app = create_app()
 
@@ -13,7 +10,7 @@ from app.routes.comment_routes import comment_bp
 from app.routes.video_routes import video_bp
 from app.routes.auth_routes import user_bp
 
-# Register blueprints
+
 app.register_blueprint(ml_bp, url_prefix="/ml")
 app.register_blueprint(comment_bp, url_prefix="/comments")
 app.register_blueprint(video_bp, url_prefix='/video')
@@ -24,5 +21,5 @@ def get_data():
     return {"message": "Hello from Flask!", "status": "success"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))  # Cloud Run requires port 8080
+    port = int(os.getenv("PORT", 8080)) 
     socketio.run(app, host="0.0.0.0", port=port, debug=os.getenv("DEBUG", "False").lower() == "true")
