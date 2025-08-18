20c3d93c1a0b7187821e3da96a2216b6c29ea8e2
mayur7891 <113599835+mayur7891@users.noreply.github.com>
2025-08-18 09:37:24 +0530

    GCP

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 class Config:
     """Configuration settings for the Flask app."""
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    JWT_SECRET_KEY = "your_jwt_secret_key_here"
-    MONGO_URI = "mongodb+srv://Ash781:mayur123@dummy.76pbm.mongodb.net/dummy?retryWrites=true&w=majority"
-
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
